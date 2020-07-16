1b07a3836bcaef80f48cff6d064d289cd299b36a
Jay Simoni <jsimoni@ipipeline.com>
2020-07-15 22:55:17 -0400

    packer & ansible using ntlmv2

--- imagepipeline/ec2-userdata.ps1
+++ imagepipeline/ec2-userdata.ps1
@@ -1,55 +1,43 @@
 <powershell>
 
-write-output "Running User Data Script"
-write-host "(host) Running User Data Script"
-
 Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
 
-# Don't set this before Set-ExecutionPolicy as it throws an error
-$ErrorActionPreference = "stop"
+# First, make sure WinRM can't be connected to
+netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new enable=yes action=block
 
-#Write-Output "updating window firewall"
-#New-NetFirewallRule -DisplayName "anyanyout" -Direction Outbound -LocalPort any -Protocol TCP -Action Allow
-#New-NetFirewallRule -DisplayName "anyanyin" -Direction inbound -LocalPort any -Protocol TCP -Action Allow
-Remove-Item "c:\windows\System32\GroupPolicy" -Recurse -Force
-gpupdate /force
+# Delete any existing WinRM listeners
+winrm delete winrm/config/listener?Address=*+Transport=HTTP  2>$Null
+winrm delete winrm/config/listener?Address=*+Transport=HTTPS 2>$Null
 
+# Install cmdlet for working with GPO Policies
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PolicyFileEditor -Force
-Set-PolicyFileEntry -Path 'C:\Windows\System32\GroupPolicy\Machine\Registry.pol' -Key 'Software\Policies\Microsoft\Windows\WinRM\Service' -ValueName 'AllowBasic' -Data '1' -Type 'DWord'
-#Uninstall-Module -Name PolicyFileEditor -Force
 
-# Remove HTTP listener
-Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
-
-#Set-Item WSMan:\localhost\MaxTimeoutms 1800000
-#Set-Item WSMan:\localhost\Service\Auth\Basic $true
-
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
+# Create SSL Cert
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $ENV:COMPUTERNAME
 New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 
-# WinRM
-write-output "Setting up WinRM"
-write-host "(host) setting up WinRM"
+# Create a new WinRM listener and configure
+winrm create winrm/config/Listener?Address=*+Transport=HTTPS
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="2048"}'
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+winrm set winrm/config/service '@{MaxConcurrentOperationsPerUser="12000"}'
+winrm set winrm/config/service/auth '@{Negotiate="true"}'
+winrm set winrm/config/Listener?Address=*+Transport=HTTPS "@{Port=`"5986`";Hostname=`"$($ENV:COMPUTERNAME)`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
 
-cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-#cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-#cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-#cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-#cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
+# Configure UAC to allow privilege elevation in remote shells via GPO
+Set-PolicyFileEntry -Path 'C:\Windows\System32\GroupPolicy\Machine\Registry.pol' -Key 'SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -ValueName 'LocalAccountTokenFilterPolicy' -Data '1' -Type 'DWord'
 
-New-NetFirewallRule -DisplayName 'Windows Remote Management (HTTPS-In)' -Name 'Windows Remote Management (HTTPS-In)' -Group "Windows Remote Management" -Description "Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]" -Profile Any -LocalPort 5986 -Protocol TCP
+# Configure and restart the WinRM Service
+Stop-Service -Name WinRM
+Set-Service -Name WinRM -StartupType Automatic
+Start-Service -Name WinRM
 
-#cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-#cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
-cmd.exe /c net stop winrm
-cmd.exe /c sc config winrm start= auto
-cmd.exe /c net start winrm
+#Create firewall rule via GPO to open the necessary port(s)
+New-NetFirewallRule -PolicyStore localhost -DisplayName 'Windows Remote Management (HTTPS-In)' -Name 'Windows Remote Management (HTTPS-In)' -Group "Windows Remote Management" -Description "Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]" -Profile Any -LocalPort 5986 -Protocol TCP
+gpupdate /force
 
-
+# Uninstall cmdlet for working with GPO Policies
+Uninstall-Module -Name PolicyFileEditor -Force
 
 </powershell>
--- imagepipeline/scripts/CreateServerAdminUser.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Creating secondary ServerAdmin account"
-Write-Host "Getting Administrator Password"
-# $AdminPassword = Get-SSMParameter -Name 'AdminPassword' -WithDecryption $true
-$AdminPasswordValue = ConvertTo-SecureString "LMnp7ZYE<F6!whmK" -AsPlainText -Force
-#$AdminPasswordValue = "LMnp7ZYE<F6!whmK"
-#Write-Host "Administrator Password: $AdminPasswordValue"
-New-LocalUser "ServerAdmin" -Password $AdminPasswordValue -FullName "Server Admin" -Description "Server Admin." -PasswordNeverExpires
-Add-LocalGroupMember -Group "Administrators" -Member "ServerAdmin"
