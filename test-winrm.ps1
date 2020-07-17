6b88965f5683566afabf8b29bc3501b599f00b9b
Jay Simoni <jsimoni@ipipeline.com>
2020-07-17 10:43:13 -0400

    updated authentication to negotiate

--- test-winrm.ps1
+++ test-winrm.ps1
@@ -4,4 +4,4 @@ $User = "Administrator"
 $PWord = ConvertTo-SecureString -String "XXXXXXXXXXXXXXXXXXXXX" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 $session = New-PSSessionOption  -SkipCACheck -SkipCNCheck
-Enter-PSSession -ComputerName "54.85.176.122" -UseSSL -Credential $Credential -Authentication "Basic" -SessionOption $session
+Enter-PSSession -ComputerName "54.85.176.122" -UseSSL -Credential $Credential -Authentication "Negotiate" -SessionOption $session
